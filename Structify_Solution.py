91cf4661560b019e03967f913677a69f822e31a8
aptekedar <144830309+aptekedar@users.noreply.github.com>
2024-02-05 19:18:03 -0500

    Update Structify_Solution.py

--- Structify_Solution.py
+++ Structify_Solution.py
@@ -1,22 +1,23 @@
+#Main function that takes in the two input lists
 def intersectingChords(chordRadians, chordPoints):
-
-
-    startPointsdict1 = {}
     
+    startPointsdict1 = {}
     endPointsdict2 = {}
     
+    # Populate dictionaries with indices of "s" and "e" labels in chordPoints
     for index, val in enumerate(chordPoints):
         if 's' in val:
             startPointsdict1[val] = index
             
         elif 'e' in val:
             endPointsdict2[val] = index
-        
+
+    # Sort the dictionaries based on keys (labels)
     myKeys1 = list(startPointsdict1.keys())
     myKeys1.sort()
     sorted_dict1 = {i: startPointsdict1[i] for i in myKeys1}
      
-     
+    # Create a new list 'sortedChords' by interleaving values from chordRadians
     myKeys2 = list(endPointsdict2.keys())
     myKeys2.sort()
     sorted_dict2 = {i: endPointsdict2[i] for i in myKeys2}
@@ -39,7 +40,7 @@ def intersectingChords(chordRadians, chordPoints):
     remembered = set()
     output = 0
     
-    
+    # Count the number of intersecting chords
     for i in range(0, len(sortedChords), 2):
         for j in range(0, len(sortedChords), 2):
             if i == j:
